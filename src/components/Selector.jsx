c43faef82615fa7f0176dcc15e92ad410a4811fe
Vaibhav prakash <vp1997dav@gmail.com>
2021-08-21 22:42:56 +0530

    changed selector button style, now hover effect ui is inhanced

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -14,19 +14,19 @@ const Selector = () => {
     return (
             <div className='select'>
                 <div className='selector'>
-                    <span title='Header' onClick={() =>setElement('head')}><FontAwesomeIcon icon={faHeading}/></span>
-                    <span title='Bold text' onClick={() =>setElement('bold')}><FontAwesomeIcon icon={faBold}/></span>
-                    <span title='Italic text' onClick={() =>setElement('italic')}><FontAwesomeIcon icon={faItalic}/></span>
-                    <span title='Block Quote' onClick={() =>setElement('quote')}><FontAwesomeIcon icon={faQuoteLeft}/></span>
-                    <span title='Link' onClick={() =>setElement('link')}><FontAwesomeIcon icon={faLink}/></span>
-                    <span title='Code' onClick={() =>setElement('code')}><FontAwesomeIcon icon={faCode}/></span>
-                    <span title='Image' onClick={() =>setElement('img')}><FontAwesomeIcon icon={faImage}/></span>
-                    <span title='Un-ordered List' onClick={() =>setElement('listul')}><FontAwesomeIcon icon={faListUl}/></span>
-                    <span title='Ordered List' onClick={() =>setElement('listol')}><FontAwesomeIcon icon={faListOl}/></span>
+                    <button title='Header' onClick={() =>setElement('head')}><FontAwesomeIcon icon={faHeading}/></button>
+                    <button title='Bold text' onClick={() =>setElement('bold')}><FontAwesomeIcon icon={faBold}/></button>
+                    <button title='Italic text' onClick={() =>setElement('italic')}><FontAwesomeIcon icon={faItalic}/></button>
+                    <button title='Block Quote' onClick={() =>setElement('quote')}><FontAwesomeIcon icon={faQuoteLeft}/></button>
+                    <button title='Link' onClick={() =>setElement('link')}><FontAwesomeIcon icon={faLink}/></button>
+                    <button title='Code' onClick={() =>setElement('code')}><FontAwesomeIcon icon={faCode}/></button>
+                    <button title='Image' onClick={() =>setElement('img')}><FontAwesomeIcon icon={faImage}/></button>
+                    <button title='Un-ordered List' onClick={() =>setElement('listul')}><FontAwesomeIcon icon={faListUl}/></button>
+                    <button title='Ordered List' onClick={() =>setElement('listol')}><FontAwesomeIcon icon={faListOl}/></button>
                 </div>
                 <div className="showpreview-div">
-                    <span ttle='Serach Markdown Syntax' className='showpreview'><FontAwesomeIcon icon={faMars} /></span>
-                    <span title='Preview' className='showpreview' onClick={(e) =>showPreview(e,state,setState)}><FontAwesomeIcon icon={faEye} /></span>
+                    <button ttle='Serach Markdown Syntax' className='showpreview'><FontAwesomeIcon icon={faMars} /></button>
+                    <button title='Preview' className='showpreview' onClick={(e) =>showPreview(e,state,setState)}><FontAwesomeIcon icon={faEye} /></button>
                 </div>
             </div>
     )
