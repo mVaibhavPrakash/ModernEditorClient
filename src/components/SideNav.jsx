1a5b1f847b948d32909b5af8b89e66140f071c4f
Vaibhav prakash <vp1997dav@gmail.com>
2021-09-20 12:56:40 +0530

    added sidebar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,8 +1,9 @@
 import '../css/sideNav.css'
+import clickNav from '../js/clickNav'
 const SideNav = () => {
     return (
         <div className='sideBar'>
-            <button id="sideBar-button" onClick={()=>{document.getElementById('edit').className='editor';document.getElementsByClassName('nav')[0].style.width='100vw';}}>X <span>Close</span></button>
+            <button id="sideBar-button" onClick={clickNav}>X <span>Close</span></button>
             <div id='sideBar-content'>
                 <div id='d'>
                     <h2>Are you ready to publish?</h2>
@@ -11,12 +12,24 @@ const SideNav = () => {
                 <div className='sideBar-content-div'>
                     <h3>SEO Title (Optional)</h3>
                     <p style={{wordBreak:'break-word'}}>The SEO Title will be shown in place of your Title on search engine results pages, such as a Google search. SEO titles between 40 and 50 characters with commonly searched words have the best click-through-rates.</p>
-                    <textarea type="text" placeholder='Enter meta title' />
+                    <textarea type="text" placeholder='Enter meta title' id='textarea-seo-title'/>
                 </div>
                 <div className='sideBar-content-div'>
                     <h3>SEO Description (Optional)</h3>
                     <p style={{wordBreak:'break-word'}}>The SEO Description will be used in place of your Subtitle on search engine results pages. Good SEO descriptions utilize keywords, summarize the story and are between 140-156 characters long.</p>
-                    <textarea type="text" placeholder='Enter meta description'/>
+                    <textarea type="text" placeholder='Enter meta description' id='textarea-seo-desc'/>
+                </div>
+                <div className="sideBar-content-div">
+                    <h3>Disable comments?</h3>
+                    <p><input type="checkbox" name="Yes" id="check" /> Yes</p>
+                </div>
+                <div className="sideBar-content-div">
+                    <h3>Disable comments?</h3>
+                    <p><input type="checkbox" name="Yes" id="check" /> Yes</p>
+                </div>
+                <div className="sideBar-content-div">
+                    <h3>Disable comments?</h3>
+                    <p><input type="checkbox" name="Yes" id="check" /> Yes</p>
                 </div>
             </div>
         </div>
